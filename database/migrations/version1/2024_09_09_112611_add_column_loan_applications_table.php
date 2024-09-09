e759ae16d1ec2fce1c73e0a991f04788c9642eff
lupenza <54394832+lupenza@users.noreply.github.com>
2024-09-09 11:35:40 +0300

    fix migration

--- database/migrations/version1/2024_09_09_112611_add_column_loan_applications_table.php
+++ database/migrations/version1/2024_09_09_112611_add_column_loan_applications_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('loan_applications', function (Blueprint $table) {
-            $table->interger('loan_type')->nullable()->default(1)->comment(" 1 =cash , 2 => paylater");
+            $table->integer('loan_type')->nullable()->default(1)->comment(" 1 =cash , 2 => paylater");
             $table->integer('device_id')->nullable();
             $table->softDeletes();
             
