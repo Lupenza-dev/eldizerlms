ac0d07cad734694810944f23300679c874f75cc2
lupenza-dev <lupenza10@gmail.com>
2026-07-25 18:11:16 +0300

    api

--- routes/api.php
+++ routes/api.php
@@ -25,60 +25,73 @@ use App\Http\Controllers\HomeController as TestController;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-Route::post('upload-loans',[TestController::class,'uploadLoans']);
-Route::get('verify-vrp',[NmbController::class,'verifyVrp']);
-Route::get('get-assignments',[HomeController::class,'getAssignments']);
+Route::post('upload-loans', [TestController::class, 'uploadLoans']);
+Route::get('verify-vrp', [NmbController::class, 'verifyVrp']);
+Route::get('get-assignments', [HomeController::class, 'getAssignments']);
 
-Route::post('V1/user-authentication',[AuthController::class,'userLogin']);
-Route::post('V1/recover-password',[AuthController::class,'recoverPassword']);
-Route::post('V1/reset-password',[AuthController::class,'resetPassword']);
-Route::post('V1/customer-registration',[CustomerController::class,'store']);
-Route::post('V1/loan-calculator',[LoanApplicationController::class,'loanCalculator']);
-Route::get('V1/get-colleges',[HomeController::class,'getColleges']);
-Route::get('V1/get-regions',[HomeController::class,'getRegions']);
-Route::get('V1/get-districts/{region_id}',[HomeController::class,'getDistricts']);
-Route::get('V1/get-wards/{district_id}',[HomeController::class,'getWards']);
-Route::get('V1/get-terms',[HomeController::class,'getTerms']);
-Route::get('V1/delete-user',[HomeController::class,'deleteUser']);
-Route::group(['prefix'=>'V1','middleware'=>'auth:api'], function(){
-    Route::get('get-devices',[HomeController::class,'getDevices']);
-    Route::post('student-registration',[CustomerController::class,'storeStudent']);
-    Route::post('loan-application',[LoanApplicationController::class,'loanApplication']);
-    Route::post('loan-calculator',[LoanApplicationController::class,'loanCalculator']);
-    Route::post('get-loans',[HomeController::class,'getLoans']);
-    Route::post('get-payments',[HomeController::class,'getPayments']);
-    Route::get('get-agents',[HomeController::class,'getAgents']);
-    Route::post('change-password',[AuthController::class,'changePassword']);
+Route::post('V1/user-authentication', [AuthController::class, 'userLogin']);
+Route::post('V1/recover-password', [AuthController::class, 'recoverPassword']);
+Route::post('V1/reset-password', [AuthController::class, 'resetPassword']);
+Route::post('V1/customer-registration', [CustomerController::class, 'store']);
+Route::post('V1/loan-calculator', [LoanApplicationController::class, 'loanCalculator']);
+Route::get('V1/get-colleges', [HomeController::class, 'getColleges']);
+Route::get('V1/get-regions', [HomeController::class, 'getRegions']);
+Route::get('V1/get-districts/{region_id}', [HomeController::class, 'getDistricts']);
+Route::get('V1/get-wards/{district_id}', [HomeController::class, 'getWards']);
+Route::get('V1/get-terms', [HomeController::class, 'getTerms']);
+Route::get('V1/delete-user', [HomeController::class, 'deleteUser']);
+Route::group(['prefix' => 'V1', 'middleware' => 'auth:api'], function () {
+    Route::get('get-devices', [HomeController::class, 'getDevices']);
+    Route::post('student-registration', [CustomerController::class, 'storeStudent']);
+    Route::post('loan-application', [LoanApplicationController::class, 'loanApplication']);
+    Route::post('loan-calculator', [LoanApplicationController::class, 'loanCalculator']);
+    Route::post('get-loans', [HomeController::class, 'getLoans']);
+    Route::post('get-payments', [HomeController::class, 'getPayments']);
+    Route::get('get-agents', [HomeController::class, 'getAgents']);
+    Route::post('change-password', [AuthController::class, 'changePassword']);
 });
 
-Route::group(['prefix'=>'V2'], function(){
-    Route::post('user-authentication',[AuthController::class,'userLogin']);
-    Route::post('user-registration',[RegistrationController::class,'registerUser']);
-    Route::post('user-registration-address',[RegistrationController::class,'registerUserAddress']);
-    Route::post('user-registration-college',[RegistrationController::class,'registerUserCollege']);
-    Route::post('user-registration-image',[RegistrationController::class,'registerUserImage']);
-    Route::post('subscribe',[NmbController::class,'subscribe']);
-    Route::get('get-colleges',[HomeController::class,'getColleges']);
-    Route::get('get-regions',[HomeController::class,'getRegions']);
-  
-    Route::get('get-groups-ads',[HomeController::class,'getGroups']);
-    Route::get('get-adverts',[HomeController::class,'getAdverts']);
-    Route::get('get-districts/{region_id}',[HomeController::class,'getDistricts']);
-    Route::get('get-wards/{district_id}',[HomeController::class,'getWards']);
-    Route::group(['middleware'=>'auth:api'], function(){
-        Route::get('get-devices',[HomeController::class,'getDevices']);
-        Route::post('student-registration',[CustomerController::class,'storeStudent']);
-        Route::post('loan-application',[LoanApplicationController::class,'loanApplication']);
-        Route::post('loan-calculator',[LoanApplicationController::class,'loanCalculator']);
-        Route::post('get-loans',[HomeController::class,'getLoans']);
-        Route::post('get-payments',[HomeController::class,'getPayments']);
-        Route::get('get-agents',[HomeController::class,'getAgents']);
-        Route::post('change-password',[AuthController::class,'changePassword']);
-        Route::post('complete-registration',[RegistrationController::class,'completeRegistration']);
-        Route::post('submit-assignment',[HomeController::class,'submitAssignment']);
-        Route::get('get-assignments',[HomeController::class,'getAssignments']);
+Route::group(['prefix' => 'V2'], function () {
 
-      
+    //new
+    // Route::post('user-authentication', [AuthController::class, 'userLogin']);
+    Route::post('recover-password', [AuthController::class, 'recoverPassword']);
+    Route::post('reset-password', [AuthController::class, 'resetPassword']);
+    Route::post('customer-registration', [CustomerController::class, 'store']);
+    Route::post('loan-calculator', [LoanApplicationController::class, 'loanCalculator']);
+    // Route::get('get-colleges', [HomeController::class, 'getColleges']);
+    // Route::get('get-regions', [HomeController::class, 'getRegions']);
+    // Route::get('get-districts/{region_id}', [HomeController::class, 'getDistricts']);
+    // Route::get('get-wards/{district_id}', [HomeController::class, 'getWards']);
+    Route::get('get-terms', [HomeController::class, 'getTerms']);
+    Route::get('delete-user', [HomeController::class, 'deleteUser']);
+
+
+    //end
+    Route::post('user-authentication', [AuthController::class, 'userLogin']);
+    Route::post('user-registration', [RegistrationController::class, 'registerUser']);
+    Route::post('user-registration-address', [RegistrationController::class, 'registerUserAddress']);
+    Route::post('user-registration-college', [RegistrationController::class, 'registerUserCollege']);
+    Route::post('user-registration-image', [RegistrationController::class, 'registerUserImage']);
+    Route::post('subscribe', [NmbController::class, 'subscribe']);
+    Route::get('get-colleges', [HomeController::class, 'getColleges']);
+    Route::get('get-regions', [HomeController::class, 'getRegions']);
+
+    Route::get('get-groups-ads', [HomeController::class, 'getGroups']);
+    Route::get('get-adverts', [HomeController::class, 'getAdverts']);
+    Route::get('get-districts/{region_id}', [HomeController::class, 'getDistricts']);
+    Route::get('get-wards/{district_id}', [HomeController::class, 'getWards']);
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::get('get-devices', [HomeController::class, 'getDevices']);
+        Route::post('student-registration', [CustomerController::class, 'storeStudent']);
+        Route::post('loan-application', [LoanApplicationController::class, 'loanApplication']);
+        Route::post('loan-calculator', [LoanApplicationController::class, 'loanCalculator']);
+        Route::post('get-loans', [HomeController::class, 'getLoans']);
+        Route::post('get-payments', [HomeController::class, 'getPayments']);
+        Route::get('get-agents', [HomeController::class, 'getAgents']);
+        Route::post('change-password', [AuthController::class, 'changePassword']);
+        Route::post('complete-registration', [RegistrationController::class, 'completeRegistration']);
+        Route::post('submit-assignment', [HomeController::class, 'submitAssignment']);
+        Route::get('get-assignments', [HomeController::class, 'getAssignments']);
     });
-  
 });
